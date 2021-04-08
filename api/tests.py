1b35998367ec07a5dc37ff329b45d2e7f46d550c
ali <weplutus.1@gmail.com>
2021-04-08 21:00:16 +0300

    tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+import json
+from graphene_django.utils.testing import GraphQLTestCase
+
+
+class MyFancyTestCase(GraphQLTestCase):
+    def test_query_posts(self):
+          True
+      #   response = self.query(
+      #       '''
+      #       query{
+      #       posts {
+      #       description
+      #       id
+      #       addedBy{
+      #           username
+      #           id
+      #           }
+      #       }
+      #       }
+      #       ''',
+      #   )
+      #   content = json.loads(response.content)
+      #   self.assertResponseNoErrors(response)
